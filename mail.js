14494c892499a54cd592f62a8e743e3d5cd1d212
Mobb autofixer <git@mobb.ai>
2023-09-05 13:48:06 +0000

    mobb fix commit: 0b20bbdb-e630-41db-a3ae-33f19bba455c

--- mail.js
+++ mail.js
@@ -2,48 +2,44 @@ var nodemailer = require('nodemailer');
 
 const { gen } = require('n-digit-token');
 
-
-
-
 function sendauthcode(email){
   console.log(email)
 
     return new Promise((resolve,reject) => {
         let code = gen(6);
- var transporter = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-          user: 'yourEmail',
-          pass: 'yourPassword'
-        }
-      });
-      
-      var mailOptions = {
-        from: 'yourEmail',
-        to: email,
-        subject: 'dummyorg authentication',
-        text: 'your authentication is: '+ code +' please dont share this with anyone'
-      };
-      
-      transporter.sendMail(mailOptions, function(error, info){
-        if (error) {
-          console.log(error);
-          return null;
-        } else {
-          console.log('Email sent: ' + info.response);
-          resolve(code);
-        }
-      }); 
-      return code;
+
+        var transporter = nodemailer.createTransport({
+            service: 'gmail',
+            auth: {
+
+              user: process.env.EMAIL_USER, /*n0*/
+              pass: process.env.EMAIL_PASS /*n1*/
+            }
+          });
+
+        var mailOptions = {
+
+          from: process.env.EMAIL_USER, /*n2*/
+          to: email,
+          subject: 'dummyorg authentication',
+          text: 'your authentication is: '+ code +' please dont share this with anyone'
+        };
+
+        transporter.sendMail(mailOptions, function(error, info){
+          if (error) {
+
+            console.log(error);
+            return null; /*n3*/
+          } else {
+
+            console.log('Email sent: ' + info.response);
+            resolve(code);
+          }
+        }); 
+
+        return code; /*n4*/
     })
-    
-
-
-   
 }
-
-
-
 
 module.exports = {sendauthcode};
 
